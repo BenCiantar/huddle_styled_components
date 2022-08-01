17e4c9f45f54a8b6f53ce3b0ac10a288c58a9080
Ben Ciantar <benjamin.ciantar@kambi.com>
2022-08-01 11:05:36 +0200

    Created a global styles file and implemented for body

--- tutorial/src/components/Header.js
+++ tutorial/src/components/Header.js
@@ -3,7 +3,8 @@ import { StyledHeader } from "./styles/Header.styled";
 export default function Header() {
   return (
     // Passing prop into component
-    <StyledHeader bg="red">
+    // <StyledHeader bg="red">
+    <StyledHeader>
       <h1>Hubble</h1>
     </StyledHeader>
   );
--- tutorial/src/components/styles/Header.styled.js
+++ tutorial/src/components/styles/Header.styled.js
@@ -3,8 +3,13 @@ import styled from "styled-components";
 export const StyledHeader = styled.header`
   // Accessing prop passed into component
   //   background-color: ${(props) => props.pg};
+
   // Can also be destructured
-  background-color: ${({ bg }) => bg};
+  //   background-color: ${({ bg }) => bg};
+
+  // Using theme
+  //   background-color: ${({ theme }) => theme.colors.header};
+
   padding: 40px 0;
 
   //   Nested styles
